8098ac7eec9c7c18242682bb41fb09c170818eae
nathan <phs960418@gmail.com>
2021-02-20 01:23:54 +0900

    디버그

--- 01_JAVA/Java1/src/OKJavaGoInHome.java
+++ 01_JAVA/Java1/src/OKJavaGoInHome.java
@@ -8,19 +8,21 @@ public class OKJavaGoInHome {
     public static void main(String[] args){
         // 남이 만들어 놓은 코드를 활용해 IoT 프로그램 만들기
         // 어떤 일이 시간 순서로 일어나야하는지 생각하기
+        String id = "JAVA APT 507";
+
         // 1. Elevator call
-        Elevator myElevator = new Elevator("JAVA APT 507");
+        Elevator myElevator = new Elevator(id);
         myElevator.callForUp(1);
 
         // 2. Security Off
-        Security mySecurity = new Security("JAVA APT 507");
+        Security mySecurity = new Security(id);
         mySecurity.off();
 
         // 3. Light ON
-        Lighting hallLamp = new Lighting("JAVA APT 507 / Hall Lamp");
+        Lighting hallLamp = new Lighting(id + " / Hall Lamp");
         hallLamp.on();
 
-        Lighting floorLamp = new Lighting("JAVA APT 507 / Floor Lamp");
+        Lighting floorLamp = new Lighting(id+ " / Floor Lamp");
         floorLamp.on();
 
     }
