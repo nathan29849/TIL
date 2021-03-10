30d369cd9ff9e1e64ed2717fb772ff4b9bdb9aed
nathan <phs960418@gmail.com>
2021-03-10 22:29:42 +0900

    링크드리스트 문제

--- 03_Python/sparta_algorithm/week_2/05_get_linked_list_sum.py
+++ 03_Python/sparta_algorithm/week_2/05_get_linked_list_sum.py
@@ -23,24 +23,34 @@ class LinkedList:
 
 def get_linked_list_sum(linked_list_1, linked_list_2):
     # 구현해보세요!
-    sum_1 = []
-    sum_2 = []
+    sum_1 = 0
+    sum_2 = 0
     total_sum = 0
     # while문을 중첩은 아니지만 두 번 연속 쓰는건 좀 비효율적인가..? -> 정답이랑 비교 해보자!
+    # while을 두 번씩 쓴 이유 : 각 linked list들이 얼마의 길이를 갖고 있는지 모르니까! GOOD
+    # 중복이 되는 부분이 많으므로 함수로 따로 만들어주어도 됨!
+    # 예시
+    # def get_sum(linked_list):
+    #     node = linked_list.head
+    #     sum = 0
+    #     while node is not None: # 6 7 8
+    #         sum = sum*10 + node.data
+    #         node = node.next
+    #     return sum
+    
     node_1 = linked_list_1.head
     node_2 = linked_list_2.head
 
-    while node_1 is not None:
-        sum_1.append(node_1.data)
+    while node_1 is not None: # 6 7 8
+        sum_1 = sum_1*10 + node_1.data
         node_1 = node_1.next
 
+
     while node_2 is not None:
-        sum_2.append(node_2.data)
+        sum_2 = sum_2*10 + node_2.data
         node_2 = node_2.next
 
-    for i in range(len(sum_1)):
-        total_sum += sum_1[i] * (10**(len(sum_1)-i-1))
-        # total_sum = sum_2[i] * (10**(len(sum_2)-i))
+    total_sum = sum_1 + sum_2
     return total_sum
 
 linked_list_1 = LinkedList(6)
