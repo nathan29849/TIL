70d1c68be2827799a273b73ad9b5c244dbcb4e0b
nathan <phs960418@gmail.com>
2021-03-12 01:04:08 +0900

    이진탐색 (무작위숫자찾기전까지)

---
+++ 03_Python/sparta_algorithm/week_2/06_is_existing_target_number_binary.py
@@ -0,0 +1,30 @@
+finding_target = 14
+finding_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+
+def is_existing_target_number_binary(target, array):
+    # 구현해보세요!
+    start = 0
+    finish = len(array) - 1
+    # count = 0
+    # 겹치는 순간부터는 finish가 start보다 먼저 오게되어 애초에 값을 찾을 수 없음.
+    # 언제까지 while문을 돌려야할지 미리미리 생각하고 코드를 짜자.
+    while start <= finish:
+        # count += 1
+        mid = (start + finish) // 2 # 이 연산자를 통해 나머지 없이 몫만 나타낼 수 있음.
+        if finish + 1 < target:
+            return False
+        if (array[mid] == target):
+            # print(count)
+            return True
+
+        elif (array[mid] < target):
+            start = mid+1
+        else:
+            finish = mid-1
+
+    return False
+
+
+result = is_existing_target_number_binary(finding_target, finding_numbers)
+print(result)
