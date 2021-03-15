405388a4b3e48104afdfdfd4909bd92c6bac2d88
nathan <phs960418@gmail.com>
2021-03-15 14:47:12 +0900

    생활코딩 JAVA CLASS

---
+++ 01_JAVA/Java1/ProjectOOP/src/MyOOP.java
@@ -0,0 +1,18 @@
+
+
+public class MyOOP {
+    public static void main(String[] args) {
+
+        //..
+        Print_3.delimiter = "----";
+        Print_3.A();
+        Print_3.B();
+        Print_3.A();
+        Print_3.B();
+        //..
+        Print_3.delimiter = "****";
+        Print_3.A();
+        Print_3.B();
+    }
+
+}
---
+++ 01_JAVA/Java1/ProjectOOP/src/Print_3.java
@@ -0,0 +1,16 @@
+public class Print_3 {
+    public static String delimiter = "";
+
+    public static void A() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+
+    public static void B() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+
+    }
+}
