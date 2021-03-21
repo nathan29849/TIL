e5055887750ef1834423b2cae00a7ed19b88d140
nathan <phs960418@gmail.com>
2021-03-21 14:53:03 +0900

    생활코딩 JAVA  생성자와 this

--- 01_JAVA/Java1/ProjectOOP/src/MyOOP.java
+++ 01_JAVA/Java1/ProjectOOP/src/MyOOP.java
@@ -2,13 +2,13 @@
 
 public class MyOOP {
     public static void main(String[] args) {
-        Print p1 = new Print();
-        p1.delimiter = "----";
+        Print p1 = new Print("----");
+//        p1.delimiter = "----";
         p1.A();
         p1.B();
         //..
-        Print p2 = new Print();
-        p2.delimiter = "****";
+        Print p2 = new Print("****");
+//        p2.delimiter = "****";
         p2.A();
         p2.B();
         //..
--- 01_JAVA/Java1/ProjectOOP/src/Print.java
+++ 01_JAVA/Java1/ProjectOOP/src/Print.java
@@ -1,14 +1,16 @@
 public class Print {
     public String delimiter = "";
-
+    public Print(String delimiter) {
+        this.delimiter = delimiter;
+    }
     public void A() {
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("A");
         System.out.println("A");
     }
 
     public void B() {
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("A");
         System.out.println("A");
 
