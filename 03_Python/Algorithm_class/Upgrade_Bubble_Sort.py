584f0b2d86b68e633ff0bc4248b5c1f8be2a3b2f
nathan <phs960418@gmail.com>
2021-04-08 00:39:00 +0900

    Algorithm python class

---
+++ 03_Python/Algorithm_class/Upgrade_Bubble_Sort.py
@@ -0,0 +1,16 @@
+a=[3, 44, 38, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50]
+
+n = len(a)
+
+
+for i in range(n-1, 0, -1):    # 마지막 원소는 자동으로 정렬!
+    sorted_list = True
+    for j in range(i):
+        if a[j] >= a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+            sorted_list = True
+        
+        
+    if sorted_list == False:
+        break
+print(a)
--- 03_Python/sparta_algorithm/week_5/test.py
+++ 03_Python/sparta_algorithm/week_5/test.py
@@ -1,5 +1,16 @@
-for i in range(10):
-    if i == 0:
-        break
+data = [1, 9, 10, 4, 2, 4, 3, 5]
+n = len(data)
+maxContigSum = []   
+# data[0] ~ data[i]까지의 원소들에 대하여, data[i]에서 끝나는 연속하는 원소들의 합 중에서 최댓값
+maxContigSum[0] = data[0]
+for i in range(1, n):
+    if (maxContigSum[i-1] > 0):
+        maxContigSum[i] = maxContigSum[i-1] + data[i]
     else:
-        print(i)
+        maxContigSum[i] = data[i]
+    print(maxContigSum)
+
+maxSum = maxContigSum[0]
+for i in range(1, n):
+    if (maxSum < maxContigSum[i]):
+        maxSum = maxContigSum
