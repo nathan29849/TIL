0cc96483e6318f9a5df6d134781b3ec0b054c06f
nathan <phs960418@gmail.com>
2021-04-08 17:47:21 +0900

    binarySearch

--- 03_Python/Algorithm_class/Upgrade_Bubble_Sort.py
+++ 03_Python/Algorithm_class/Upgrade_Bubble_Sort.py
@@ -4,7 +4,7 @@ n = len(a)
 
 
 for i in range(n-1, 0, -1):    # 마지막 원소는 자동으로 정렬!
-    sorted_list = True
+    sorted_list = False
     for j in range(i):
         if a[j] >= a[j+1]:
             a[j], a[j+1] = a[j+1], a[j]
