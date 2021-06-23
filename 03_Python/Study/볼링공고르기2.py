fe6703db3022282f1df0df60929d651793ec9f30
nathan <phs960418@gmail.com>
2021-06-23 09:50:59 +0900

    study 볼링공고르기 재풀이

--- 03_Python/Study/볼링공고르기2.py
+++ 03_Python/Study/볼링공고르기2.py
@@ -3,15 +3,15 @@ from sys import stdin
 
 
 def Pick(n, ballList):
-    combination = (n*(n-1))//2     # nC2 = n*n-1 // 2
+    C = []
+    print("ballList :", ballList)
     count = 0
-    for i in range(n-1):
-        if ballList[i] == ballList[i+1]:
-            count += 1
-    
-
-    
-    return combination - count
+    for i in range(n):
+        for j in range(i, n):
+            if ballList[i] != ballList[j]:
+                C.append((ballList[i], ballList[j]))
+    print(C)
+    return len(C)
 
 
 n, m = map(int, stdin.readline().split())
@@ -22,10 +22,11 @@ result = Pick(n, ballList)
 print(result)
 
 # 5 3
-# 2 3 2 3 2
+# 2 3 2 3 2  -> 2, 2, 2, 3, 3
 
 # 8 5
 # 1 5 4 3 2 4 5 2
 
 # 풀이애서 m이 필요가 없게 되어서 뭔가 좀 찝찝함... 아무리 봐도 m은 필요가 없는 듯 함..
-# 그리디를 사용했는가? : 음.. 같은것의 개수를 무조건 빼준다.
+# 그리디를 사용했는가? : 값이 같지 않으면 무조건 선택 
+# 시간 복잡도 : O(N^2)
