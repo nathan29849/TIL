41bd8fcd9f92fd314667a467d093740a1e01feec
nathan <phs960418@gmail.com>
2021-06-24 17:14:32 +0900

    study 만들 수 없는 금액 재풀이

--- 03_Python/Study/만들수없는금액.py
+++ 03_Python/Study/만들수없는금액.py
@@ -1,23 +1,15 @@
 from sys import stdin
-import copy
 
 def search(n, coin):
-    num = coin[-1]
-    C = [0 for i in range(num+1)]
-    C[0] = 1
-    for i in range(1, num+1):
-        copy_coin = copy.deepcopy(coin)
-        for x in copy_coin:
-            copy_coin.pop(copy_coin.index(x))
-            if x == i:
-                C[i] = 1
-                break
-            elif i > x:
-                if C[i-x] != 0:
-                    C[i] += C[i-x]
-        if C[i] == 0:
-            return i
-    return C            
+    goal = 1
+    for i in coin:
+        if goal < i:
+            break
+        goal += i
+        print("goal :", goal)
+    
+    return goal
+
 
 
 n = int(stdin.readline())
