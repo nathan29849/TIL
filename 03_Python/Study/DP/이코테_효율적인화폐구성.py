8659a90ef9d3a5813e06aca2c8a9bebdfd1de224
nathan <phs960418@gmail.com>
2021-08-03 20:27:33 +0900

    이코테 DP 실전 5번 효율적 화폐 구성

--- 03_Python/Study/DP/이코테_효율적인화폐구성.py
+++ 03_Python/Study/DP/이코테_효율적인화폐구성.py
@@ -1,4 +1,4 @@
-# 이코테 실전문제 3번 개미전사
+# 이코테 실전문제 5번 효율적인 화폐 구성
 from sys import stdin
 f = stdin.readline
 n, m = map(int, f().split())
@@ -12,11 +12,20 @@ def solution(wallet, n, m):
     for i in range(n):
         if wallet[i] <= m:
             money[wallet[i]] = 1
-    
-    # money = [0, 0, 1, 1, 0, 0, 0, 0, 0, ...]
-    for j in range(money[0], m+1):
-        
 
+    for j in range(1, m+1):
+        temp = []
+        if money[j] == 0:
+            for x in wallet:
+                if j >= x:
+                    if money[j-x] != 0:
+                        temp.append(money[j-x])
+            if len(temp) != 0:
+                money[j] = min(temp) + 1
+    print(money)    
+    return money[m] if money[m] > 0 else -1
+
+print(solution(wallet, n, m))
 
 
     
