94fa22a41c5c79cda90e5c775bce1e1bf8f3673a
nathan29849 <phs960418@gmail.com>
2021-10-12 23:27:29 +0900

    개똥벌레 푸는눙

--- 03_Python/Study/이진탐색/백준_개똥벌레.py
+++ 03_Python/Study/이진탐색/백준_개똥벌레.py
@@ -2,21 +2,49 @@
 from sys import stdin
 from collections import deque
 n, h = map(int, input().split())
-# stalagmite = [] # 석순
-# stalactite = [] # 종유석
-cave = deque()
+mite = [] # 석순
+tite = [] # 종유석
+
 heights = [0 for _ in range(h+1)]
 for i in range(n):
     temp = int(input())
     if i % 2 == 0:
         # 석순
-        cave.append([0, temp])
+        mite.append(temp) 
     else:
         # 종유석
-        cave.append([h-temp, h])
+        tite.append(temp)
 
-mid = h//2
-for i in range(n):
-    low, high = cave.popleft()
+mite.sort()   # 석순 오름차순 정렬
+tite.sort()   # 종유석 오름차순 정렬
+
+for i in range(1, h+1):
+    count = 0
+    for j in range(n//2):
+        now = mite[j]
+        if i > now:
+            count += 1
+        else:
+            heights[i] = n//2 - count
+            break
+
+for i in range(h, 0, -1):
+    count = 0
+    for j in range(n//2):
+        now = tite[j]
+        # h - i + 1 : 전체 높이에서 현재 높이를 뺀 후 +1
+        if h-i+1 > now: 
+            count += 1
+        else:
+            heights[i] += n//2 - count
+            break
+
+min_num = min(heights[1:])
+result = 0
+for i in range(1, h+1):
+    if min_num == heights[i]:
+        result += 1
+
+print(min_num, result)
 
 
